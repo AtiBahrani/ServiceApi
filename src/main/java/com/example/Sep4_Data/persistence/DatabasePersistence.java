6013a4b31c7a19fbc2d599641dfdbde68ad3be4a
AtiBahrani <45685013+AtiBahrani@users.noreply.github.com>
2020-05-21 22:31:19 +0200

    cloud database

--- src/main/java/com/example/Sep4_Data/persistence/DatabasePersistence.java
+++ src/main/java/com/example/Sep4_Data/persistence/DatabasePersistence.java
@@ -10,9 +10,9 @@ public class DatabasePersistence implements DatabaseAdaptor {
     private MyDatabase db;
 
     private static final String DRIVER = "org.postgresql.Driver";
-    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "machintosh";
+    private static final String URL = "jdbc:postgresql://balarama.db.elephantsql.com:5432/lwavwwgi";
+    private static final String USER = "lwavwwgi";
+    private static final String PASSWORD = "B1jwM3F8_fo289D9wXPxNHLEgVDYXZxr";
 
     public DatabasePersistence() {
         try {
@@ -37,16 +37,16 @@ public class DatabasePersistence implements DatabaseAdaptor {
     @Override
     public synchronized void addSensorData(Sensor data) throws SQLException {
         //insert data in three tables sensorType, measurement and unit
-        String sensor = "INSERT INTO SEP4_Source.SensorType(sensorName) VALUES (?);";
-        String measurement = "INSERT INTO SEP4_Source.Measurement(value,timestamp) VALUES (?,?);";
-        String unit = "INSERT INTO SEP4_Source.Unit(unitName) VALUES (?);";
+        String sensor = "INSERT INTO SensorType(sensorName) VALUES (?);";
+        String measurement = "INSERT INTO Measurement(value,timestamp) VALUES (?,?);";
+        String unit = "INSERT INTO Unit(unitName) VALUES (?);";
 
         db.update(sensor, data.getSensorName());
         db.update(measurement, data.getValue(), new Timestamp(data.getTimestamp()));
         db.update(unit, data.getUnitType());
 
         //key look up for getting the id in order to put to the in between relations
-        String sensorTypeId = "SELECT sensorType_ID FROM SEP4_Source.SensorType WHERE sensorName =?;";
+        String sensorTypeId = "SELECT sensorType_ID FROM SensorType WHERE sensorName =?;";
 
         int sID, mID, uID = 0;
 
@@ -54,12 +54,12 @@ public class DatabasePersistence implements DatabaseAdaptor {
 
         ArrayList<Object[]> sIds = db.query(sensorTypeId, data.getSensorName());
         sID = Integer.parseInt(sIds.get(0)[0].toString());
-        String sensorTable = "INSERT INTO SEP4_Source.Sensor(sensorType_ID )VALUES (?);";
+        String sensorTable = "INSERT INTO Sensor(sensorType_ID )VALUES (?);";
         db.update(sensorTable, sID);
 
         //get sensorId
         int sensor_ID = 0;
-        String sensorId = "SELECT sensor_ID FROM SEP4_Source.Sensor join SEP4_Source.SensorType ON " +
+        String sensorId = "SELECT sensor_ID FROM Sensor join SensorType ON " +
                 "SensorType.sensorType_ID =Sensor.sensorType_ID WHERE sensorName =?;";
 
         ArrayList<Object[]> senID = db.query(sensorId, data.getSensorName());
@@ -67,21 +67,21 @@ public class DatabasePersistence implements DatabaseAdaptor {
 
 
         //query to get measurementId to put in SensorMeasurement
-        String measurementId = "SELECT measurement_ID FROM SEP4_Source.Measurement WHERE value =? and timestamp=?;";
+        String measurementId = "SELECT measurement_ID FROM Measurement WHERE value =? and timestamp=?;";
 
         ArrayList<Object[]> mIds = db.query(measurementId, data.getValue(), new Timestamp(data.getTimestamp()));
         mID = Integer.parseInt(mIds.get(0)[0].toString());
-        String sensorMeasurements = "INSERT INTO SEP4_Source.SensorMeasurement(measurement_ID ,sensor_ID)VALUES (?,?);";
+        String sensorMeasurements = "INSERT INTO SensorMeasurement(measurement_ID ,sensor_ID)VALUES (?,?);";
 
         db.update(sensorMeasurements, mID, sensor_ID);
 
         // get unit_ID
-        String unitId = "SELECT unit_ID FROM SEP4_Source.Unit WHERE unitName =?;";
+        String unitId = "SELECT unit_ID FROM Unit WHERE unitName =?;";
 
         ArrayList<Object[]> uIds = db.query(unitId, data.getUnitType());
         uID = Integer.parseInt(uIds.get(0)[0].toString());
 
-        String sensorUnits = "INSERT INTO SEP4_Source.SensorUnit(unit_ID,sensor_ID )VALUES (?,?);";
+        String sensorUnits = "INSERT INTO SensorUnit(unit_ID,sensor_ID )VALUES (?,?);";
 
 
         db.update(sensorUnits, uID, sensor_ID);
@@ -99,11 +99,11 @@ public class DatabasePersistence implements DatabaseAdaptor {
     public List<Sensor> getData() throws SQLException {
         //join between 6 tables to get the data for sensor object
         String dataSql = "SELECT  unitName ,sensorName,Measurement.value , Measurement.timestamp " +
-                "FROM SEP4_Source.Unit JOIN SEP4_Source.SensorUnit ON SEP4_Source.Unit.unit_ID = SEP4_Source.SensorUnit.unit_ID " +
-                "JOIN SEP4_Source.Sensor ON SEP4_Source.SensorUnit.sensor_ID = Sensor.sensor_ID " +
-                "JOIN SEP4_Source.SensorType ON SEP4_Source.Sensor.sensorType_ID = SensorType.sensorType_ID " +
-                "INNER JOIN SEP4_Source.SensorMeasurement s ON Sensor.sensor_ID = s.sensor_ID " +
-                "INNER JOIN SEP4_Source.Measurement ON s.measurement_ID = Measurement.measurement_ID;";
+                "FROM Unit JOIN SensorUnit ON Unit.unit_ID = SensorUnit.unit_ID " +
+                "JOIN Sensor ON SensorUnit.sensor_ID = Sensor.sensor_ID " +
+                "JOIN SensorType ON Sensor.sensorType_ID = SensorType.sensorType_ID " +
+                "INNER JOIN SensorMeasurement s ON Sensor.sensor_ID = s.sensor_ID " +
+                "INNER JOIN Measurement ON s.measurement_ID = Measurement.measurement_ID;";
 
         List<Sensor> list = new ArrayList<>();
         //select query
